8e6ec08d9a8d1b3a0283cde0a71a13485d36be98
Dwiyanawijaya51017007 <48231241+Dwiyanawijaya51017007@users.noreply.github.com>
2020-03-24 16:50:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Dwi Yana Wijaya, 51017007, SI')
 })
 
 app.listen(PORT, HOST)
